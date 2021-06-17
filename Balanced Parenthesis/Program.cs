14317f950052d815c6ee69812edc6b612d52a6d8
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2021-06-17 21:02:51 +0300

    Add project files.

---
+++ Balanced Parenthesis/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> parenthes = new Stack<char>();
+
+
+            foreach (var symbol in input)
+            {
+                if (parenthes.Any())
+                {
+                    char check = parenthes.Peek();
+
+                    if (check == '{' && symbol == '}')
+                    {
+                        parenthes.Pop();
+                        continue;
+                    }
+
+
+                    else if (check == '[' && symbol == ']')
+                    {
+                        parenthes.Pop();
+                        continue;
+                    }
+
+
+                    else if (check == '(' && symbol == ')')
+                    {
+                        parenthes.Pop();
+                        continue;
+                    }
+                }
+                parenthes.Push(symbol);
+            }
+            Console.WriteLine(!parenthes.Any() ? "YES" : "NO");
+        }
+    }
+}
